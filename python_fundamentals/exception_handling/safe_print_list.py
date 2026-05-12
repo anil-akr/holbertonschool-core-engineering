7c386199e44d5db8496c3c7766c4e8ae82174f1e
anil-akr <4nil000@protonmail.com>
2026-05-12 09:02:13 +0000

    m

---
+++ python_fundamentals/exception_handling/safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def safe_print_list(my_list=[], x=0):
+    n = 0
+
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            n = n + 1
+        except:
+            break
+    print()
+    return n
