2948fad4eda3f3b3d64a67267f7b629ffc622b66
anil-akr <4nil000@protonmail.com>
2026-05-29 13:00:25 +0000

    Fix T0

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -1,13 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Module that reads a UTF-8 text file and prints it."""
 
 
 def read_file(filename=""):
-    """Read and print the content of a UTF-8 text file.
-
-    Args:
-        filename (str): Name of the file to read.
-    """
-    with open(filename, 'r', encoding='utf-8') as rf:
-        fc = rf.read()
-        print(fc, end="")
+    """Read and print the content of a UTF-8 text file."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
