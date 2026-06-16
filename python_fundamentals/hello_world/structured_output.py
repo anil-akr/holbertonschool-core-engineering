ac7879f50de6ad1b5bb6f924c72461f140ebaacb
anil-akr <4nil000@protonmail.com>
2026-06-16 15:03:08 +0200

    T1

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+resultat = f"Pi approx: {3.14159:.2f}"
+valid = (3.14 > 3)
+print("Language: Python")
+print("Version: 3")
+print(resultat)
+print(f"Computation valid: {valid}")
