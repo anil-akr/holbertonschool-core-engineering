c1c4873d714be36fcd5779dbf39a64c5460cf6bf
anil-akr <4nil000@protonmail.com>
2026-06-30 13:39:13 +0200

    m

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python3
-
-number = __import__('random').randint(-10000, 10000)
+"""Display the last digit of a number using its absolute value."""
 
 
 def print_last_digit(number):
     last_digit = abs(number) % 10
-
-    if number < 0:
-        last_digit = -last_digit
-
-    else:
-        return last_digit
+    print(last_digit)
+    return last_digit
