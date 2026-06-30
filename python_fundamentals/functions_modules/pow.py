a81606ae6a15a0f2b5683a819be27c25e0199cec
anil-akr <4nil000@protonmail.com>
2026-06-30 13:52:01 +0200

    m

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-"""Compute a raised to the power of b without the ** operator."""
+"""Compute a raised to the power of b without the exponent operator."""
 
 
 def pow(a, b):
     result = 1
-    for _ in range(b):
+    for _ in range(abs(b)):
         result *= a
+    if b < 0:
+        result = 1 / result
     return result
