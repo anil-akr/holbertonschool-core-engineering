168c9bb7f9df20d456d9a230aad4756ad0cb3697
anil-akr <4nil000@protonmail.com>
2026-07-01 11:26:51 +0200

    m

--- python_fundamentals/core_data_structures/main.py
+++ python_fundamentals/core_data_structures/main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-add_tuple = __import__('add_tuple').add_tuple
+update_dictionary = __import__('update_dictionary').update_dictionary
 
-print(add_tuple((1, 89), (88, 11)))
-print(add_tuple((1, 89), (1, )))
-print(add_tuple((1, 89), ()))
-print(add_tuple((), ()))
+d = {'language': 'C', 'number': 89, 'track': 'Low level'}
+print(update_dictionary(d, 'language', 'Python'))
+print(update_dictionary(d, 'city', 'San Francisco'))
