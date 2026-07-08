f3bbaeff270d689f17224223d5fc060c34033db1
anil-akr <4nil000@protonmail.com>
2026-07-08 14:05:09 +0200

    m

---
+++ python_oop/inheritance/2-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Defines a Rectangle class that inherits from BaseGeometry."""
+
+BaseGeometry = __import__('base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class Rectangle that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
